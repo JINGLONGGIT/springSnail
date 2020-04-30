87e23d3db5b81af3399251ea5920f20eed264205
JINGLONGGIT <jinglong_email@163.com>
2020-04-30 14:49:17 +0800

    [新增] 新增线程同步机制包装类

---
+++ demo/locker.h
@@ -0,0 +1,126 @@
+#ifndef __LOCKER_H_
+#define __LOCKER_H_
+
+// 线程同步机制包装类
+#include "global.h"
+
+// 信号量
+class CSem
+{
+public:
+    // 创建并初始化信号量
+    CSem()
+    {
+        if (sem_init(&m_sem, 0, 0) != 0)
+        {
+            // 构造函数没有返回值，可以通过抛出异常来判断错误
+            throw std::exception();
+        }
+    }
+
+    // 销毁信号量
+    ~CSem()
+    {
+        sem_destroy(&m_sem);
+    }
+
+    // 等待信号量
+    bool wait()
+    {
+        return sem_wait(&m_sem) == 0;
+    }
+
+    // 增加信号量
+    bool post()
+    {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+
+// 互斥锁
+class CLocker
+{
+public:
+    // 创建并初始化互斥锁
+    CLocker()
+    {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0)
+        {
+            throw std::exception();
+        }
+    }
+
+    // 销毁互斥锁
+    ~CLocker()
+    {
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    // 获取互斥锁
+    bool lock()
+    {
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+
+    // 释放互斥锁
+    bool unlock()
+    {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+
+// 条件变量
+class Cond
+{
+public:
+    // 创建并初始化条件变量
+    Cond()
+    {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0)
+        {
+            throw std::exception();
+        }
+
+        if (pthread_cond_init(&m_cond, NULL) != 0)
+        {
+            pthread_mutex_destroy(&m_mutex);
+            throw std::exception();
+        }
+    }
+
+    // 销毁条件变量
+    ~Cond()
+    {
+        pthread_mutex_destroy(&m_mutex);
+        pthread_cond_destroy(&m_cond);
+    }
+
+    // 等待条件变量
+    bool wait()
+    {
+        int ret = 0;
+        pthread_mutex_lock(&m_mutex);
+        ret = pthread_cond_wait(&m_cond, &m_mutex);
+        pthead_mutex_unlock(&m_mutex);
+        retrun ret == 0;
+    }
+
+    // 唤醒等待条件变量的线程
+    bool signal()
+    {
+        return pthread_cond_signal(&m_cond) == 0;
+    }
+
+private:
+    pthread_t_mutex_t m_mutex;
+    phthread_cond_t m_cond;
+};
+
+#endif
+
