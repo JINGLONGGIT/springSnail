85248611fbd598a46d4c864d2d6ff7d267e93f88
JINGLONGGIT <jinglong_email@163.com>
2020-05-02 08:50:47 +0800

    [修改] 将普通的服务端代码改为文件传输服务端

--- demo/demo_socket_server.cpp
+++ demo/demo_socket_server.cpp
@@ -20,34 +20,43 @@
 // 文件传输系统
 int main(int argc, char * argv [ ])
 {
-    int serv_sock, clnt_sock;
-    struct sockaddr_in serv_addr, clnt_addr;
-    char buffer[BUFSIZE];
+    const char *filename = "/mnt/hgfs/gitSpace/weatherDC/remotedata/20200319185105.txt";
+    FILE *fp = fopen(filename, "wb");
+    if (fp == NULL)
+    {
+        printf("文件不存在\n");
+        return -1;
+    }
 
-    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int listenfd, connfd;
+    struct sockaddr_in serv_addr, clnt_addr;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(1234);
-    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    listen(serv_sock, 20);
+    listen(listenfd, 20);
 
     socklen_t clnt_addr_size = sizeof(clnt_addr);
-    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    connfd = accept(listenfd, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
 
-    int i32RecvLen = recv(clnt_sock, buffer, sizeof(buffer), 0);
-    if (i32RecvLen > 0)
+    char buffer[BUFSIZE] = {'\0'};
+    int nCount;
+    while ((nCount = fread(buffer, 1, BUFSIZE, fp)) > 0)
     {
-        printf("recv data: %s", buffer);
+        send(connfd, buffer, BUFSIZE - 1, 0);
     }
+    
+    shutdown(connfd, SHUT_WR);                  // 断开输出流，向客户端发送FIN
+    recv(connfd, buffer, sizeof(buffer), 0);
 
-    send(clnt_sock, buffer, i32RecvLen, 0);
-
-    close(clnt_sock);
-    close(serv_sock);
-
+    fclose(fp);
+    close(connfd);
+    close(listenfd);
     return 0;
 }
 
